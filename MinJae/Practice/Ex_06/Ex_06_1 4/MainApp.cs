75decb8f3c3f319892233d3af95f6637f3851062
3oh6un <leewh0829@gmail.com>
2024-07-14 15:26:39 +0900

    뭔지 모름 일단 커밋

---
+++ MinJae/Practice/Ex_06/Ex_06_1 4/MainApp.cs
@@ -0,0 +1,18 @@
+namespace MinJae.Practice.Ex_06.EX_06_1;
+
+public class MainApp
+{
+    static void _Main(string[] args)
+    {
+        Console.WriteLine("수를 입력하세요.");
+        string input = Console.ReadLine();
+        double arg = Convert.ToDouble(input);
+
+        Console.WriteLine("결과: {0}", Square(arg));
+    }
+
+    static double Square(double arg)
+    {
+        return arg * arg;
+    }
+}
